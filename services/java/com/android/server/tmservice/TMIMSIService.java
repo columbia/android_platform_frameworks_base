c2ce0cd5c616975366fab62f4781da484afed193
Kangkook Jee <aixer77@gmail.com>
2013-12-30 12:28:29 -0500

    merge

--- services/java/com/android/server/tmservice/TMIMSIService.java
+++ services/java/com/android/server/tmservice/TMIMSIService.java
@@ -6,28 +6,47 @@ import android.content.Context;
 import dalvik.system.Taint;
 
 /**
- * Class for TMeasure IMSI service 
+ * Class for TMeasure IMSI service.
+ * 
+ * IMSI is fixed 15-digit length. 
+ * 
+ * 3-digit Mobile Country Code(MCC) + 
+ * 3-digit Mobile Network Code(MNC) + 
+ * 9-digit Mobile Station Identification Number(MSIN).
+ *  
  * @author jikk
  *
  */
 public class TMIMSIService extends TMService {
   protected static final String TAG = "TMIMSIService";
   int tag = -1;
-
+  
+  private int mcc = 310;
+  private int mnc = 260;
+  private int msin = 1;
+  private String imsi;
+  
   public String getIMSI() {
     Log.e("JIKK-IMSIService", "getIMSI called:0");
-    return "310260000000002";
+    return imsi;
   }
 
   public int getTag() {
-      return Taint.TAINT_IMSI;
+      return tag;
   }
 
-  protected void run_over(int port_, String cmd) {
-    tag = 0;
-
+  public void next() {
+      //TODO: implement some fuzzing logic here.
+      tag += 1;
+      msin += 1;
+      imsi = String.format("%d%d%09d", mcc, mnc, msin);
+  }
+  
+  protected void run_over(int port_, String subcmd) {
     //signals that we begin another iteration
-    Taint.TMLog("runover |" + Taint.incTmCounter() + "| | | " + Integer.toHexString(tag));
+      
+    next();     
+    Taint.TMLog("runover |" + Taint.incTmCounter() + "| | | " + Integer.toHexString(tag));    
   }
 
   public TMIMSIService(Context context) {
@@ -35,5 +54,8 @@ public class TMIMSIService extends TMService {
 
     Log.v(TAG, "TMIMSIService started" );
     registerTmSvc(TAG, this);
+    
+    //init. private(service specific) variables.
+    imsi = String.format("%d%d%09d", mcc, mnc, msin);    
   }
 }
--- tmservice/java/com/android/tmservice/TMIMSIManager.java
+++ tmservice/java/com/android/tmservice/TMIMSIManager.java
@@ -30,15 +30,25 @@ public class TMIMSIManager {
     }
   }
   
+  public void next() {
+      try {
+        mContext.next();
+      } catch (RemoteException re) {
+        Log.e(TAG, "next() Remote Exception" + re.toString());
+      } catch (Exception e) {
+        Log.e(TAG, "next() Exception" + e.toString());
+      }
+  }
+  
   public int getTag() {
       try {
         int ret = mContext.getTag();
         return ret;
       } catch (RemoteException re) {
-        Log.e(TAG, "getTag Remote Exception" + re.toString());
+        Log.e(TAG, "getTag() Remote Exception" + re.toString());
         return 0;
       } catch (Exception e) {
-        Log.e(TAG, "getTag Exception" + e.toString());
+        Log.e(TAG, "getTag() Exception" + e.toString());
         return 0;
       }
   }
